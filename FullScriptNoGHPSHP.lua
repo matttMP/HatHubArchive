d4aec156ecc4982b6c941b602317214856173092
matttMP <66797927+matttMP@users.noreply.github.com>
2020-09-22 15:46:25 -0500

    Update FullScriptNoGHPSHP.lua

--- FullScriptNoGHPSHP.lua
+++ FullScriptNoGHPSHP.lua
@@ -158,8 +158,6 @@ ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
 spoof(game.Players.LocalPlayer, "MaximumSimulationRadius", 1000)
 spoof(game.Players.LocalPlayer, "SimulationRadius", 1000)
-sethiddenproperty(game.Players.LocalPlayer,"MaximumSimulationRadius",math.huge)
-sethiddenproperty(game.Players.LocalPlayer,"SimulationRadius",1.0000000331814e+32)
 
 local NetworkAccess = coroutine.create(function()
     settings().Physics.AllowSleep = false
